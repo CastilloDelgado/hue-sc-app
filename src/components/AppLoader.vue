d3872131b442b33c3fa1c6a8f308b18595791953
Marco Castillo <marcocastillo1997@gmail.com>
2023-09-18 14:21:56 -0600

    Adding activity indicator

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const props = defineProps({
+    interval: {
+        typeof: Number,
+        default: 100
+    }
+})
+
+const stringToShow = ref('')
+const characters = ["|", "/", "---", "\\"]
+
+var i = 0;
+const animation = () => {
+    setTimeout(() => {
+        stringToShow.value = characters[i]
+        if(i === 3){
+            i = 0
+        } else {
+            i++
+        }
+        if(i < 100000){
+            animation()
+        }
+    }, props.interval)
+}
+
+onMounted(() => {
+    animation()
+})
+
+</script>
+
+<template>
+    <p class="w-6 text-center tracking-[-0.20em]">{{ stringToShow }}</p>
+</template>
--- src/components/LightsSettings.vue
+++ src/components/LightsSettings.vue
@@ -1,19 +1,20 @@
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { HueServices } from "../services/HueServices"
 import { useBridgeStore } from "../stores/BridgeStore"
 import { storeToRefs } from 'pinia';
 import LightInfo from './LightInfo.vue'
-
+import AppButton from './AppButton.vue';
+import AppLoader from './AppLoader.vue';
 
 const bridgeStore = useBridgeStore()
 const { lights } = storeToRefs(bridgeStore)
+const loading = ref(false)
 
-
-onMounted(() => {   
+const fetchLights = () => {
+    loading.value = true
     HueServices.getLights()
         .then((response) => {
-            console.log(response.data)
             const lightsData = Object.entries(response.data).map(([key, lightData]) => ({
                 id: parseInt(key),
                 ...lightData
@@ -24,6 +25,14 @@ onMounted(() => {
         .catch((error) => {
             console.log(error)
         })
+        .then(() => {
+            loading.value = false
+        })
+}
+
+
+onMounted(() => {   
+    fetchLights()
 })
 
 </script>
@@ -31,6 +40,10 @@ onMounted(() => {
 <template>
     <div class="mt-2">
         <p class="text-2xl font-bold text-neutral-800 mb-4">Lights Settings</p>
+        <div class="mb-2 flex">
+            <AppButton title="Get lights" :action="fetchLights"  />
+            <AppLoader class="text-red-500 ml-2" v-if="!loading"/>
+        </div>
         <div class="flex overflow-auto">
             <div v-for="light in lights" :key="light.id">
                 <LightInfo :light="light" />
