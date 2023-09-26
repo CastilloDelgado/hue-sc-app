2571eedab23d17f35c672f3157b45438591eb5c1
Marco Castillo <marcocastillo1997@gmail.com>
2023-09-26 17:41:44 -0600

    Adding step info

--- src/components/LightInfo.vue
+++ src/components/LightInfo.vue
@@ -9,10 +9,10 @@ defineProps({
 
 <template>
     <div class="border-2 border-black mr-2 px-2 py-1">
-        <div class="flex">
-            <b>ID: </b> <p>{{ light.id }}</p>
+        <div class="flex flex-row">
+            <b>ID: </b><p>{{ light.id }}</p>
         </div>
-        <div class="flex">
+        <div class="flex flex-col lg:flex-row ">
             <b>Name: </b> <p>{{ light.name }}</p>
         </div>
     </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,10 +7,25 @@ import SequenceCreator from "../components/SequenceCreator.vue";
 
 <template>
   <div class="p-4">
-    <div class="flex flex-col md:flex-row">
-      <HueSettings class="w-full md:w-1/3" />
-      <LightsSettings class="w-full md:w-2/3" />
+    <div class="flex flex-col lg:flex-row">
+      <div class="w-full lg:w-1/3 pr-6" >
+        <div>
+          <p class="text-3xl font-bold text-red-500">Step 1</p>
+          <p class="text-xs text-justify mb-1">First, we need to link the app with your bridge, to do this we are trying to automatically detect the IP and ID of your Bridge in your local network. Be sure that you are at the same network that the one your bridge is connected. If we can't find your information, you can provide it manually.</p>
+          <p class="text-xs text-justify mb-1">After that, you need to create an username, for that you need to press the button on your bridge and then press "Create Username" button down below.</p>
+        </div>
+        <HueSettings/>
+      </div>
+      <div>
+        <div  class="w-full lg:w-2/3" >
+          <p class="text-3xl font-bold text-red-500">Step 2</p>
+          <p class="text-xs text-justify mb-1">After the 1st step is completed, we can test the app by getting all the lights that are linked to your bridge. This should be executed automatically when the username is created or you can execute it manually clicking the "Get Lights" button.</p>
+        </div>
+        <LightsSettings/>
+      </div>
     </div>
+    <p class="text-3xl font-bold text-red-500 mb-1">Step 3</p>
+    <p class="text-xs text-justify mb-1">This is the sequence creator. First you need to create a sequence, click on the "Create Sequence" button and then fill the form. After that, a new sequence will be shown down below. You can add steps for every light filling the state form. Get creative here and create light shows for you favorite songs!!! (Please notice that the max of request of the bridge are 8 calls per second)</p>
     <SequenceCreator />
   </div>
 </template>
