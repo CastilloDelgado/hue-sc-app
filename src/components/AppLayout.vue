b053ed63a558f4e7f3dc26669732249d7bac4770
Marco Castillo <marcocastillo1997@gmail.com>
2023-09-26 19:41:40 -0600

    Changing background color

--- src/components/AppLayout.vue
+++ src/components/AppLayout.vue
@@ -4,7 +4,7 @@ import AppHeader from './AppHeader.vue';
 </script>
 
 <template>
-    <div class="flex justify-center bg-neutral-400 min-h-screen">
+    <div class="flex justify-center bg-black min-h-screen">
         <div class="mx-5 bg-neutral-50 container">
             <AppHeader></AppHeader>
             <slot />
