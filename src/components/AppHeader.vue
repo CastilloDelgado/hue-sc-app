feef541bd61c7b127cf5e11f44bd907d63092daf
Marco Castillo <marcocastillo1997@gmail.com>
2023-09-28 17:18:52 -0600

    Adding version in app header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,12 @@
 <script setup>
-
+import { version } from "../../package.json"
 </script>
 
 <template>
     <div class="w-full pt-3">
         <div class="flex justify-center mb-3">
             <p class="text-center text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-500 via-blue-500 via-purple-500 to-red-500 w-fit underline">Hue Show Creator</p>
+            <p class="text-xs flex items-end">Version {{ version }}</p>
         </div>
         <div class="bg-gradient-to-r from-green-500 via-blue-500 via-purple-500 to-red-500 py-1">
         </div>
